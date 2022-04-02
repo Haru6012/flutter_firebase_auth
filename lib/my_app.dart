cb86fb7a25c84dd55d0fe94d5754a574f48e2972
松坂波季 <matuharu@matsuzakahakinoMacBook-Pro.local>
2022-04-03 00:25:58 +0900

    first commit

---
+++ lib/my_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/sign_in_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Firebase Auth Sample",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: SignInScreen(),
+    );
+  }
+}
